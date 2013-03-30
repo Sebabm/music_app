47ccf012920071daca876ab33a8b97c346db4a8f
SebaBM <sebastian.vitangeli@gmail.com>
2013-03-30 18:36:10 -0300

    Finish user signup

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,7 +4,7 @@ describe "StaticPages" do
   subject { page }
   describe "Home page" do
     before { visit root_path }
-    it { should have_selector('h1', text: 'Sample App') }
+    it { should have_selector('h1', text: 'Music App') }
     it { should have_selector('title', text: full_title('')) }
   end
   
